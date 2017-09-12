72cc618ab31b9eb6c778348039ced6c68695e00c
Jakob Misbach <jakobmisbach8@gmail.com>
2017-09-12 08:19:57 -0400

    Image

--- Timeline/src/com/javanerds/main/Assets.java
+++ Timeline/src/com/javanerds/main/Assets.java
@@ -3,9 +3,10 @@ package com.javanerds.main;
 import java.awt.image.BufferedImage;
 
 public class Assets {
-    public static BufferedImage odyessy;
+    public static BufferedImage odyessy, steamlogo;
 
     public static void init(){
         odyessy = ImageLoader.loadImage("/img/odyessy.jpg");
+        steamlogo = ImageLoader.loadImage("/img/steamlogo.png");
     }
 }
--- Timeline/src/com/javanerds/scenes/FirstGen.java
+++ Timeline/src/com/javanerds/scenes/FirstGen.java
@@ -17,6 +17,6 @@ public class FirstGen extends Scene{
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.odyessy, 100, 100, null);
+        g.drawImage(Assets.steamlogo, 0, 0, null);
     }
 }
