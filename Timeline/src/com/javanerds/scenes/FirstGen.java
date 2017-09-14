dc3947944ec6fd22054ddd3c34981aab287c5bf9
Jack Fireblade <balancejackfireblade@gmail.com>
2017-09-14 07:46:26 -0400

    changed gaben to become the odyessy
    yeah

--- Timeline/src/com/javanerds/scenes/FirstGen.java
+++ Timeline/src/com/javanerds/scenes/FirstGen.java
@@ -17,6 +17,6 @@ public class FirstGen extends Scene{
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.gaben, 0, 0, null);
+        g.drawImage(Assets.odyessy, 0, 0, null);
     }
 }
