79eb966901537971729803c8eaf5f473c69784fb
Jack Fireblade <balancejackfireblade@gmail.com>
2017-09-14 07:58:37 -0400

    first generation complete

--- Timeline/src/com/javanerds/scenes/FirstGen.java
+++ Timeline/src/com/javanerds/scenes/FirstGen.java
@@ -19,6 +19,14 @@ public class FirstGen extends Scene{
     public void render(Graphics g) {
         g.setFont(new Font("Comic Sans MS", 1, 20));
         g.drawString("2", 0, 20);
-        g.drawImage(Assets.odyessy, 0, 0, null);
+        g.drawImage(Assets.odyessy, 100, 175, null);
+        g.setFont(new Font("Comic Sans MS", 1, 40));
+        g.drawString("First Generation of Consoles", 200, 50);
+        g.setFont(new Font("Comic Sans MS", 1, 30));
+        g.drawString("The Atari Odyessy", 500, 200);
+        g.drawString("1974", 500, 250);
+        g.drawString("console with classic Pong game", 500, 300);
+        g.drawString("used knobs to control sticks on screen", 500, 350);
+
     }
 }
