afbc3eb1525b2d6518a25680809fa69911f6e732
Jakob Misbach <jakobmisbach8@gmail.com>
2017-09-14 08:52:57 -0400

    time

--- Timeline/src/com/javanerds/scenes/Gaben.java
+++ Timeline/src/com/javanerds/scenes/Gaben.java
@@ -17,7 +17,7 @@ public class Gaben extends Scene{
             Scene.setScene(Timeline.getSteam());
         }
         try {
-            Thread.sleep(1000);
+            Thread.sleep(10);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
