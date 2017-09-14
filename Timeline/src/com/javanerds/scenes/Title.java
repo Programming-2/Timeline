4b8f8ecd918963b7c4636d84c8213d5e4835b015
Jack Fireblade <balancejackfireblade@gmail.com>
2017-09-14 07:51:01 -0400

    put in numbers

--- Timeline/src/com/javanerds/scenes/Title.java
+++ Timeline/src/com/javanerds/scenes/Title.java
@@ -15,6 +15,8 @@ public class Title extends Scene {
 
     @Override
     public void render(Graphics g) {
+        g.setFont(new Font("Comic Sans MS", 1, 20));
+        g.drawString("1", 0, 20);
         g.setFont(new Font("Comic Sans MS", 1, 70));
         g.drawString("The History of Game Development", 60, 200);
         g.setFont(new Font("Comic Sans MS", 1, 40));
