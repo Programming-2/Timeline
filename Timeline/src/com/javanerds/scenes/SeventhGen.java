5d13dedf629b74ac9a9f857a2f0cdda3212ce9c6
Will Anderson <wllmscttandrsn@gmail.com>
2017-09-14 09:00:45 -0400

    :eggplant:

--- Timeline/src/com/javanerds/scenes/SeventhGen.java
+++ Timeline/src/com/javanerds/scenes/SeventhGen.java
@@ -1,5 +1,6 @@
 package com.javanerds.scenes;
 
+import com.javanerds.main.Assets;
 import com.javanerds.main.Timeline;
 
 import java.awt.*;
@@ -26,6 +27,14 @@ public class SeventhGen extends Scene{
     public void render(Graphics g) {
         g.setFont(new Font("Comic Sans MS", 1, 20));
         g.drawString("8", 0, 20);
-
+        g.drawImage(Assets.ps3, 650, 100, null);
+        g.drawImage(Assets.xbox360, 700, 420, null);
+        g.drawImage(Assets.wii, 100, 200, null);
+        g.setFont(new Font("Comic Sans MS", 1, 60));
+        g.drawString("Seventh Generation", 400, 50);
+        g.setFont(new Font("Comic Sans MS", 1, 30));
+        g.drawString("Playstation 3, 2006", 720, 340);
+        g.drawString("Xbox 360, 2005", 770, 600);
+        g.drawString("Wii, 2006", 130, 550);
     }
 }
