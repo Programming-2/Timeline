73d529c43f3e79f91bfd59ce0279ac45b5e174b0
Jack Fireblade <balancejackfireblade@gmail.com>
2017-09-14 08:05:44 -0400

    changed title

--- Timeline/src/com/javanerds/scenes/Title.java
+++ Timeline/src/com/javanerds/scenes/Title.java
@@ -24,7 +24,7 @@ public class Title extends Scene {
         g.setFont(new Font("Comic Sans MS", 1, 20));
         g.drawString("1", 0, 20);
         g.setFont(new Font("Comic Sans MS", 1, 70));
-        g.drawString("The History of Game Development", 60, 200);
+        g.drawString("The History of Gaming Technology", 60, 200);
         g.setFont(new Font("Comic Sans MS", 1, 40));
         g.drawString("By Jarod Davies, Jakob Misbach, and Will Anderson", 120, 550);
     }
