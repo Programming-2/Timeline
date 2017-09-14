58cb14934007a43bc4cba0257f272feae0fe82a8
Will Anderson <wllmscttandrsn@gmail.com>
2017-09-14 08:34:58 -0400

    fourthgen :fire:

--- Timeline/src/com/javanerds/scenes/FourthGen.java
+++ Timeline/src/com/javanerds/scenes/FourthGen.java
@@ -1,5 +1,7 @@
 package com.javanerds.scenes;
 
+import com.javanerds.main.Assets;
+
 import java.awt.*;
 
 public class FourthGen extends Scene{
@@ -15,6 +17,13 @@ public class FourthGen extends Scene{
 
     @Override
     public void render(Graphics g) {
+        g.drawImage(Assets.genesis, 180, 100, null);
+        g.drawImage(Assets.supernes, 700, 400, null);
+        g.setFont(new Font("Comic Sans MS", 1, 60));
+        g.drawString("Fourth Generation", 400, 50);
+        g.setFont(new Font("Comic Sans MS", 1, 30));
+        g.drawString("Sega Genesis, 1989", 180, 280);
+        g.drawString("Super NES, 1991", 720, 600);
         g.setFont(new Font("Comic Sans MS", 1, 20));
         g.drawString("5", 0, 20);
 
