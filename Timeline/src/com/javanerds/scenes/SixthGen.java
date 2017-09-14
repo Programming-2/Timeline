fdee4f336924be756c4785aa478f46e96825edb4
Will Anderson <wllmscttandrsn@gmail.com>
2017-09-14 08:56:04 -0400

    sixthgen :100:

--- Timeline/src/com/javanerds/scenes/SixthGen.java
+++ Timeline/src/com/javanerds/scenes/SixthGen.java
@@ -1,5 +1,6 @@
 package com.javanerds.scenes;
 
+import com.javanerds.main.Assets;
 import com.javanerds.main.Timeline;
 
 import java.awt.*;
@@ -26,6 +27,14 @@ public class SixthGen extends Scene{
     public void render(Graphics g) {
         g.setFont(new Font("Comic Sans MS", 1, 20));
         g.drawString("7", 0, 20);
-
+        g.drawImage(Assets.gamecube, 650, 100, null);
+        g.drawImage(Assets.xbox, 700, 420, null);
+        g.drawImage(Assets.ps2, 100, 200, null);
+        g.setFont(new Font("Comic Sans MS", 1, 60));
+        g.drawString("Sixth Generation", 400, 50);
+        g.setFont(new Font("Comic Sans MS", 1, 30));
+        g.drawString("Gamecube, 2001", 720, 340);
+        g.drawString("Xbox, 2001", 770, 600);
+        g.drawString("Playstation 2, 2000", 130, 550);
     }
 }
